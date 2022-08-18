b9eff32894171fef4e6fcc9996ee572f0fb77815
xtophe38 <christophe.naud@gmail.com>
2022-08-18 09:29:37 +0200

    Change copyright

--- main.php
+++ main.php
@@ -1,19 +1,8 @@
 <?php
-// Copyright (C) 2014 Combodo SARL
-//
-//   This application is free software; you can redistribute it and/or modify	
-//   it under the terms of the GNU Affero General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   iTop is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU Affero General Public License for more details.
-//
-//   You should have received a copy of the GNU Affero General Public License
-//   along with this application. If not, see <http://www.gnu.org/licenses/>
-
+/*
+ * @copyright   Copyright (C) 2022 TeemIp
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
 
 require_once(APPROOT.'collectors/TeemIpDiscoveryIPv4Collector.class.inc.php');
 require_once(APPROOT.'collectors/TeemIpDiscoveryIPv4SubnetCollector.class.inc.php');
