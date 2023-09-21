30bc06910598e8ad845630461abb437306879430
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2023-09-21 13:04:16 +0200

    ipconfig_id does not need to be synchronised for subnets

--- src/TeemIpDiscoveryIPv4SubnetCollector.class.inc.php
+++ src/TeemIpDiscoveryIPv4SubnetCollector.class.inc.php
@@ -73,7 +73,6 @@ class TeemIpDiscoveryIPv4SubnetCollector extends Collector
 			if ($aIPv4Subnet['ipdiscovery_enabled'] == 'yes') {
 				$this->aIPv4Subnet[$index]['ip'] = $sSubnetIp;
 				$this->aIPv4Subnet[$index]['org_id'] = $aIPv4Subnet['org_id'];
-				$this->aIPv4Subnet[$index]['ipconfig_id'] = $aIPv4Subnet['ipconfig_id'];
 				$this->aIPv4Subnet[$index]['last_discovery_date'] = $aIPv4Subnet['last_discovery_date'];
 				$this->aIPv4Subnet[$index]['ping_duration'] = $aIPv4Subnet['ping_duration'];
 				$this->aIPv4Subnet[$index]['ping_discovered'] = $aIPv4Subnet['ping_discovered'];
@@ -127,25 +126,6 @@ class TeemIpDiscoveryIPv4SubnetCollector extends Collector
 	/**
 	 * @inheritdoc
 	 */
-	protected function InitProcessBeforeSynchro(): void
-	{
-		// Create IPConfig mapping table
-		$this->oIPv4AddressIPConfigMapping = new LookupTable('SELECT IPConfig', array('org_id_friendlyname'));
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	protected function ProcessLineBeforeSynchro(&$aLineData, $iLineIndex)
-	{
-		if (!$this->oIPv4AddressIPConfigMapping->Lookup($aLineData, array('org_id'), 'ipconfig_id', $iLineIndex)) {
-			throw new IgnoredRowException('Unknown IP Config');
-		}
-	}
-
-	/**
-	 * @inheritdoc
-	 */
 	public function fetch()
 	{
 		if ($this->iIndex < count($this->aIPv4Subnet)) {
@@ -153,7 +133,6 @@ class TeemIpDiscoveryIPv4SubnetCollector extends Collector
 			$aDatas['primary_key'] = $this->aIPv4Subnet[$this->iIndex]['ip'];
 			$aDatas['ip'] = $this->aIPv4Subnet[$this->iIndex]['ip'];
 			$aDatas['org_id'] = $this->aIPv4Subnet[$this->iIndex]['org_id'];
-			$aDatas['ipconfig_id'] = $this->aIPv4Subnet[$this->iIndex]['ipconfig_id'];
 			$aDatas['last_discovery_date'] = $this->aIPv4Subnet[$this->iIndex]['last_discovery_date'];
 			$aDatas['ping_duration'] = $this->aIPv4Subnet[$this->iIndex]['ping_duration'];
 			$aDatas['ping_discovered'] = $this->aIPv4Subnet[$this->iIndex]['ping_discovered'];
